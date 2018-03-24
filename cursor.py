132a5a1d42d60ba6d43e9b0db89957419c43fb4c
linluk <lukas42singer@gmail.com>
2018-03-24 20:17:50 +0100

    removed trailing whites

--- cursor.py
+++ cursor.py
@@ -8,6 +8,6 @@ class Cursor():
         self.rect = None
         self._textsurface = None
 
-    def render(self, screen, mouse_pos, p_color):    
+    def render(self, screen, mouse_pos, p_color):
         self.rect = (mouse_pos[0], mouse_pos[1], consts.CURSOR_W, consts.CURSOR_H)
         pygame.draw.rect(screen, p_color, self.rect, 0)
