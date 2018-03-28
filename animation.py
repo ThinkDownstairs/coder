1135a27fc5af0f25d082f846ec4eb51314a615da
linluk <lukas42singer@gmail.com>
2018-03-29 00:06:33 +0200

    improoved animation performance! (pre-subsurfacing)

--- animation.py
+++ animation.py
@@ -5,50 +5,38 @@ import pygame
 
 
 class Animation(object):
-    __slots__ = ('_count', '_sprite', '_current', '_idx', '_width', '_height', '_duration', '_tick', '_once', '_done')
+    __slots__ = ('_count', '_sprites', '_idx', '_width', '_height', '_duration', '_tick', '_once', '_done')
     def __init__(self, sprite: pygame.Surface, count: int = 1, fps: int = 8, once: bool = False) -> None:
         super().__init__()
         self._count = count
-        self._sprite = sprite
-        self._current = None
         self._once = once
         self._done = False
         self._idx = 0
-        self._width = self._sprite.get_width() // count
-        self._height = self._sprite.get_height()
+        self._width = sprite.get_width() // count
+        self._height = sprite.get_height()
         self._duration = 1000 // fps
+        self._sprites = [sprite.subsurface((self._width * i, 0, self._width, self._height)) for i in range(count)]
         self._tick = 0
 
     def update(self, delta: int) -> None:
         if self._done:
             return
         self._tick -= delta
-        reclip = False
         while self._tick <= 0:
-            reclip = True
             self._idx += 1
             self._tick += self._duration
-        if reclip:
-            if self._idx >= self._count:
-                self._idx %= self._count
-                self._done = self._once
-                if self._done:
-                    self._idx = self._count - 1 # ensure that we stop on the last animation frame
-            self._current = self._sprite.subsurface((self._width * self._idx, 0, self._width, self._height))
+        if self._idx >= self._count:
+            self._idx %= self._count
+            self._done = self._once
+            if self._done:
+                self._idx = self._count - 1 # ensure that we stop on the last animation frame
 
     def render(self, target: pygame.Surface, x: int, y: int) -> None:
-        if self._current is None:
-            return
-        target.blit(self._current, (x, y))
-
-    def _get_surface(self):
-        if self._current is None:
-            self._current = self._sprite.subsurface((self._width * self._idx, 0, self._width, self._height))
-        return self._current
+        target.blit(self._sprites[self._idx], (x, y))
 
     width = property(lambda s: s._width)
     height = property(lambda s: s._height)
-    surface = property(_get_surface)
+    surface = property(lambda s: s._sprites[s._idx])
     done = property(lambda s: s._done)
 
 
