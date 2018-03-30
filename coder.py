ec37623f65a04cf50dc7f2f350b9a0e6aeee4184
linluk <lukas42singer@gmail.com>
2018-03-30 12:27:33 +0200

    icon, icon-caption, spelling error in window caption

--- coder.py
+++ coder.py
@@ -9,14 +9,16 @@ import consts
 import bugreport
 import splash
 import highscore
+import locations
 
 
 def init():
     pygame.mixer.pre_init(44100, -16, 2, 4096)
     pygame.mixer.init()
     pygame.init()
-    screen = pygame.display.set_mode((consts.SCREEN_W, consts.SCREEN_H))
-    pygame.display.set_caption('CODER  a game for the #MetaGamaJam 17. - 31. March 2018')
+    pygame.display.set_icon(pygame.image.load(locations.image('coder-icon.png')))
+    screen = pygame.display.set_mode((consts.SCREEN_W, consts.SCREEN_H))#, pygame.DOUBLEBUF)
+    pygame.display.set_caption('CODER  a game for the #MetaGameJam 17. - 31. March 2018', 'c0d3r')
     pygame.mouse.set_visible(0)
     return screen
 
